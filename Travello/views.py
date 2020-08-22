e35f6e5b97be584a11b71118b621dc668d5f946a
Yagya Raj Bhatta <54733074+YagyaB@users.noreply.github.com>
2020-08-22 21:01:09 +0545

    initial commit

---
+++ Travello/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from .models import Destination
+
+# Create your views here.
+
+def index(request):
+    
+    dests = Destination.objects.all()
+    
+    """
+    dest1 = Destination()
+    dest1.name = 'Dhangadhi'
+    dest1.desc = 'City of Cricket'
+    dest1.img = 'image/destination_1.jpg'
+    dest1.price = '800'
+    #dest1.offer = False
+
+    dest2 = Destination()
+    dest2.name = 'Lumbani'
+    dest2.desc = 'Birthplace of lord Budha'
+    dest2.img = 'image/destination_2.jpg'
+    dest2.price = '900'
+    #dest2.offer = True
+
+    dest3 = Destination()
+    dest3.name = 'Kathmandu'
+    dest3.desc = 'Capital city of Nepal'
+    dest3.img = 'image/destination_3.jpg'
+    dest3.price = '1000'
+    #dest3.offer = False
+    
+    dests = [dest1, dest2, dest3]
+    """
+
+    return render(request, "index.html", {'dests': dests})
